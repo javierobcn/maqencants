9cbebdc0f89b74b89c59b2cddff9e21e5f05a429
javierobcn <javierobcn@gmail.com>
2021-02-06 23:14:52 +0100

    First Version

---
+++ theme_maqencants/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'MaqEncants Theme',
+    'description': 'MaqEncants Theme',
+    'category': 'Theme/Corporate',
+    'version': '1.0',
+    'depends': ['website', 'website_theme_install'],
+    'data': [
+        'views/assets.xml',
+        'views/layout.xml',
+    ],
+    'images': [
+        'static/description/bootswatch.png',
+        'static/description/bootswatch_screenshot.jpg',
+    ],
+}
