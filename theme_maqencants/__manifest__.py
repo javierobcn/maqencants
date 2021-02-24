d3ad0e31c53cc2ed061bcbbc0ca35fddcf2a080f
javierobcn <javierobcn@gmail.com>
2021-02-24 17:46:56 +0100

    Cookie Notice

--- theme_maqencants/__manifest__.py
+++ theme_maqencants/__manifest__.py
@@ -6,11 +6,12 @@
     'description': 'MaqEncants Theme',
     'category': 'Theme/Corporate',
     'version': '1.0',
-    'depends': ['website', 'website_sale', 'website_theme_install'],
+    'depends': ['website', 'website_sale', 'website_theme_install', 'website_cookie_notice'],
     'data': [
         'views/assets.xml',
         'views/layout.xml',
         'views/store.xml',
+        'views/website.xml',
     ],
     'images': [
         'static/description/bootswatch.png',
